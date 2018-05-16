6dfb75e3704015a10e0fab624895d3a77b0f2b7c
Daniel Fain <d6niel@protonmail.com>
2018-05-15 23:26:54 -0400

    tkinter implementation

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import secrets
 import string
-import tkinter
-
-al = string.ascii_letters + string.digits
+from tkinter import *
 
 
-def inputHandler():
-    while True:
-        try:
-            n = input('How many characters? ')
-            if int(n) > 0:
-                return int(n)
-            else:
-                raise ValueError
-        except ValueError:
-            print('Please only enter positive integers.')
+def generate(*args):
+    al = (string.ascii_letters + string.digits)
+    try:
+        value = int(length.get())
+        password.set(''.join(secrets.choice(al) for i in range(value)))
+    except ValueError:
+        pass
 
 
-pw = ''.join(secrets.choice(al) for i in range(inputHandler()))
-print(pw)
+root = Tk()
+root.title('pwgen')
+
+mainframe = Frame(root)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+password = StringVar()
+length = StringVar()
+
+length_entry = Entry(mainframe, width=7, textvariable=length)
+length_entry.grid(column=2, row=1, sticky=(W, E))
+Label(mainframe, textvariable=password).grid(column=2, row=2, sticky=(W, E))
+Button(mainframe, text="Generate", command=generate).grid(column=3, row=3, sticky=W)
+Label(mainframe, text="characters").grid(column=3, row=1, sticky=W)
+Label(mainframe, text="your password is").grid(column=1, row=2, sticky=E)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+length_entry.focus()
+root.bind('<Return>', generate)
+
+root.mainloop()
