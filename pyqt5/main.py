b7f5def7887fd7a03a10a463c55c55839dec1475
Daniel Fain <d6niel@protonmail.com>
2018-05-21 06:01:53 -0400

    work

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -2,40 +2,32 @@ import sys
 import pyperclip
 import string
 import secrets
-from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5 import QtGui, QtWidgets
 
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle('pwgen')
-        self.setWindowIcon(QtGui.QIcon('logo.png'))
-        self.menu()
+        self.title = 'pwgen'
+        self.min = 0
+        self.max = 64
         self.view()
 
-    def menu(self):
-        menu_bar = QtWidgets.QMenuBar()
-        self.setMenuBar(menu_bar)
-        file_menu = menu_bar.addMenu('File')
-
-        exit = QtWidgets.QAction(' &Exit', self)
-        exit.setShortcut('Ctrl+Q')
-        exit.setStatusTip('Close the application')
-        exit.triggered.connect(self.close_app)
-        file_menu.addAction(exit)
-
     def view(self):
-        gen_btn = QtWidgets.QPushButton('Generate', self)
-        gen_btn.resize(gen_btn.sizeHint())
-        gen_btn.move(100, 100)
-        gen_btn.clicked.connect(self.generate_password)
+        self.setWindowTitle(self.title)
+        self.generate_password()
+        self.close_app()
 
-        self.show()
+    def get_len(self):
+        i, okPressed = QtWidgets.QInputDialog.getInt(self, self.title, "Characters:", 0, self.min, self.max, 1)
+        if okPressed:
+            return i
+        else:
+            self.close_app()
 
     def generate_password(self):
         al = string.ascii_letters + string.digits
-        pyperclip.copy(''.join(secrets.choice(al) for i in range(16)))
+        pyperclip.copy(''.join(secrets.choice(al) for i in range(self.get_len())))
 
     def close_app(self):
         sys.exit()
