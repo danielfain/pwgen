9a4e9f52d066a19d704ec34b76179a0fb9dc049a
Daniel Fain <d6niel@protonmail.com>
2018-05-21 04:44:11 -0400

    work

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -11,11 +11,21 @@ class Window(QtWidgets.QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle('pwgen')
         self.setWindowIcon(QtGui.QIcon('logo.png'))
+        exit_app = QtWidgets.QAction('Exit', self)
+        exit_app.setShortcut('Ctrl+Q')
+        exit_app.setStatusTip('Close the application')
+        exit_app.triggered.connect(self.close_app)
+        self.statusBar()
+        main_menu = self.menuBar()
+        file_menu = main_menu.addMenu('&File')
+        file_menu.addAction(exit_app)
+
         self.view()
 
     def view(self):
         gen_btn = QtWidgets.QPushButton('Generate', self)
         gen_btn.resize(gen_btn.sizeHint())
+        gen_btn.move(100, 100)
         gen_btn.clicked.connect(self.generate_password)
 
         self.show()
@@ -23,6 +33,9 @@ class Window(QtWidgets.QMainWindow):
     def generate_password(self):
         al = string.ascii_letters + string.digits
         pyperclip.copy(''.join(secrets.choice(al) for i in range(16)))
+
+    def close_app(self):
+        sys.exit()
 
 
 if __name__ == '__main__':
